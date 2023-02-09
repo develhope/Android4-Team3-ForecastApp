7e012701e7f6255a05ce224e4469e003b10a11d2
kalemat00 <kalemat@hotmail.it>
2023-02-09 12:59:56 +0100

    Today Screen Implementation
    Added one fragment
    Needed to show the today screen

---
+++ app/src/main/java/co/develhope/meteoapp/Today_screen.kt
@@ -0,0 +1,21 @@
+package co.develhope.meteoapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import co.develhope.meteoapp.databinding.FragmentTodayScreenBinding
+
+
+class Today_screen : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentTodayScreenBinding =
+            FragmentTodayScreenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
