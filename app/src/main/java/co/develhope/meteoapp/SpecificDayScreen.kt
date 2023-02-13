799ed821a3ee496ac1548d6f65c27f8d7d7cd901
kalemat00 <kalemat@hotmail.it>
2023-02-13 23:51:59 +0100

    Bottom Navigation With Navigation Component
    -Removed old navigation with transaction
    -Added new navigation with nav graph
    -Bottom navigation icons switched to figma icons
    -Added a selector for button color
    -Added 4 blank fragments
    -Added navigation component dependencies in build grandle file

---
+++ app/src/main/java/co/develhope/meteoapp/SpecificDayScreen.kt
@@ -0,0 +1,25 @@
+package co.develhope.meteoapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SpecificDayScreen : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_specific_day_screen, container, false)
+    }
+
+
+}
