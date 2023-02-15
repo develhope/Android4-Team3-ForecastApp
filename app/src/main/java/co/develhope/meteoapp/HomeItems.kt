0cdb779bef9804bedaf543e69ec7e0a3bb2b91a4
Lorenzo Balestrini <lorenzo.balestrini6@gmail.com>
2023-02-15 19:26:41 +0100

    feature-home screen

--- app/src/main/java/co/develhope/meteoapp/HomeItems.kt
+++
@@ -1,5 +0,0 @@
-package co.develhope.meteoapp
-
-sealed class HomeItems{
-
-}
--- app/src/main/java/co/develhope/meteoapp/HomeScreenAdapter.kt
+++ app/src/main/java/co/develhope/meteoapp/HomeScreenAdapter.kt
@@ -2,11 +2,29 @@ package co.develhope.meteoapp
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
 
-class HomeViewHolder(view:View): RecyclerView.ViewHolder(view) {
+class HomeCardViewHolder(view:View): RecyclerView.ViewHolder(view) {
 
+    val date : TextView
+    val minDegree: TextView
+    val maxDegree: TextView
+    val windKmh: TextView
+    val rainPerc: TextView
+    val weather: ImageView
+
+    init{
+        date = view.findViewById(R.id.date_card)
+        minDegree = view.findViewById(R.id.min_degrees)
+        maxDegree = view.findViewById(R.id.max_degrees)
+        windKmh = view.findViewById(R.id.wind_kmh)
+        rainPerc = view.findViewById(R.id.rain_perc)
+        weather = view.findViewById(R.id.icon_weather)
+    }
 }
 
 class HomeScreenAdapter: RecyclerView.Adapter<HomeViewHolder>() {
