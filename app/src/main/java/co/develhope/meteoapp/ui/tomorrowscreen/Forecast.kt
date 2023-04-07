e780ea6752b9ddcbbcd77ca1a2fbd95ba132497c
kalemat00 <kalemat@hotmail.it>
2023-04-07 16:21:24 +0200

    Initial DiffUtil Implementation on TomorrowAdapter

--- app/src/main/java/co/develhope/meteoapp/ui/tomorrowscreen/Forecast.kt
+++ app/src/main/java/co/develhope/meteoapp/ui/tomorrowscreen/Forecast.kt
@@ -3,13 +3,17 @@ package co.develhope.meteoapp.ui.tomorrowscreen
 import co.develhope.meteoapp.data.domainmodel.DomainHourlyForecast
 import co.develhope.meteoapp.data.domainmodel.Place
 
-sealed class Forecast {
+sealed class Forecast() {
+    //This will make the id available on Forecast, but delegates the storage to whatever implements it.
+    abstract val id: Int
     data class HourlyForecastListItem(
-        val domainHourlyForecast: DomainHourlyForecast
+        val domainHourlyForecast: DomainHourlyForecast,
+        override val id: Int = domainHourlyForecast.date.hour
     ) : Forecast()
     
     data class TitleForecast(
         val domainHourlyForecast: DomainHourlyForecast,
-        val place: Place
+        val place: Place,
+        override val id: Int = domainHourlyForecast.date.year
     ) : Forecast()
 }
--- app/src/main/java/co/develhope/meteoapp/ui/tomorrowscreen/tomorrowadapter/TomorrowAdapter.kt
+++ app/src/main/java/co/develhope/meteoapp/ui/tomorrowscreen/tomorrowadapter/TomorrowAdapter.kt
@@ -2,16 +2,18 @@ package co.develhope.meteoapp.ui.tomorrowscreen.tomorrowadapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import co.develhope.meteoapp.R
 import co.develhope.meteoapp.databinding.HourlyForecastListItemBinding
 import co.develhope.meteoapp.databinding.HourlyForecastTitleItemBinding
 import co.develhope.meteoapp.ui.tomorrowscreen.Forecast
 
-class TomorrowAdapter(private val list: List<Forecast>) :
+class TomorrowAdapter() :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when (viewType) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        when (viewType) {
             ViewType.TITLE_FORECAST.value -> {
                 val titleBinding = HourlyForecastTitleItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
                 HourlyTitleItem(titleBinding)
@@ -22,27 +24,30 @@ class TomorrowAdapter(private val list: List<Forecast>) :
             }
             else -> throw java.lang.IllegalArgumentException("error")
         }
-    }
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (getItemViewType(position)) {
             ViewType.TITLE_FORECAST.value -> {
-                (holder as HourlyTitleItem).bindTitleItem(list[position] as Forecast.TitleForecast)
+                (holder as HourlyTitleItem).bindTitleItem(differ.currentList[position] as Forecast.TitleForecast)
             }
             ViewType.HOURLY_FORECAST_LIST_ITEM.value -> {
-                (holder as HourlyListItem).bindListItem(list[position] as Forecast.HourlyForecastListItem)
+                (holder as HourlyListItem).bindListItem(differ.currentList[position] as Forecast.HourlyForecastListItem)
             }
             else -> throw java.lang.IllegalArgumentException("error")
         }
+        holder.setIsRecyclable(false)
     }
-    override fun getItemCount(): Int = list.size
-    override fun getItemViewType(position: Int): Int {
-        return when (list[position]) {
-            is Forecast.HourlyForecastListItem -> ViewType.HOURLY_FORECAST_LIST_ITEM.value
-            is Forecast.TitleForecast -> ViewType.TITLE_FORECAST.value
-        }
+    override fun getItemCount(): Int = differ.currentList.size
+    override fun getItemViewType(position: Int): Int = when (differ.currentList[position]) {
+        is Forecast.HourlyForecastListItem -> ViewType.HOURLY_FORECAST_LIST_ITEM.value
+        is Forecast.TitleForecast -> ViewType.TITLE_FORECAST.value
     }
     private enum class ViewType(val value: Int) {
         TITLE_FORECAST(R.layout.hourly_forecast_title_item),
         HOURLY_FORECAST_LIST_ITEM(R.id.hourly_forecast_list_item_view)
     }
+    private val differCallback = object : DiffUtil.ItemCallback<Forecast>() {
+        override fun areItemsTheSame(oldItem: Forecast, newItem: Forecast): Boolean = (oldItem.id == newItem.id)
+        override fun areContentsTheSame(oldItem: Forecast, newItem: Forecast): Boolean = (oldItem == newItem)
+    }
+    val differ = AsyncListDiffer(this, differCallback)
 }
