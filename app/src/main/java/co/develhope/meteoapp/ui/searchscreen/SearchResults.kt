3aec60eac39368705946ab9d7ec8287fa1024762
kalemat00 <kalemat@hotmail.it>
2023-04-17 19:04:42 +0200

    First Implementation of Search Events

--- app/src/main/java/co/develhope/meteoapp/ui/searchscreen/SearchResults.kt
+++ app/src/main/java/co/develhope/meteoapp/ui/searchscreen/SearchResults.kt
@@ -3,6 +3,13 @@ package co.develhope.meteoapp.ui.searchscreen
 import co.develhope.meteoapp.data.domainmodel.Place
 
 sealed class SearchResults{
-    data class Results(val results: List<Place>) : SearchResults()
+    data class Results(val results: List<GetCitiesList>) : SearchResults()
     data class Errors(val errors: String) : SearchResults()
+}
+
+sealed class SearchEvent{
+    object RetrieveListFromPreferences : SearchEvent()
+    data class WritingOnSearchBar(val cityToSearch: String?) : SearchEvent()
+    data class AddCityToPreferences(val cityToAdd: Place) : SearchEvent()
+
 }
