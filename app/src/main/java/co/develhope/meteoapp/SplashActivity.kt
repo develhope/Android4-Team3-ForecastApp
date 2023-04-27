61bf26c2fa7849748553a8004733c64755343b98
Eufemia <smaldonefm@gmail.com>
2023-04-27 16:30:46 +0200

    added gif

--- app/src/main/java/co/develhope/meteoapp/SplashActivity.kt
+++ app/src/main/java/co/develhope/meteoapp/SplashActivity.kt
@@ -18,7 +18,7 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 1000)
+        }, 3600)
 
     }
 }
